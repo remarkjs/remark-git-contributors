162dc2981eb206524c3c6618564fe38e813d8949
Vincent Weevers <mail@vincentweevers.nl>
2018-06-03 22:34:22 +0200

    initial

---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const githubFromPackage = require('github-from-package')
+const getContributors = require('github-contributors')
+const injectContributors = require('remark-contributors')
+const parallel = require('run-parallel')
+const GitHub = require('github-base')
+const resolve = require('resolve')
+const path = require('path')
+const fs = require('fs')
+const plugin = require('./package.json').name
+
+module.exports = function attacher (opts) {
+  if (typeof opts === 'string') {
+    opts = { contributors: opts }
+  } else if (!opts) {
+    opts = {}
+  }
+
+  // Required scopes: public_repo, read:user
+  const token = opts.token || process.env.GITHUB_TOKEN
+  const cwd = path.resolve(opts.cwd || '.')
+
+  return function transform (root, file, callback) {
+    if (!token) {
+      file.info('no github token provided', null, `${plugin}:require-token`)
+      return callback()
+    }
+
+    const meta = getMetadata(cwd, file, opts.contributors)
+    const json = fs.readFileSync(path.join(cwd, 'package.json'), 'utf8')
+    const pkg = JSON.parse(json)
+    const slug = githubFromPackage(pkg).split('/').slice(-2).join('/')
+    const github = new GitHub({ token })
+
+    getContributors(slug, { token }, function (err, contributors) {
+      if (err) return callback(err)
+
+      if (file.stem && file.stem.toLowerCase() === 'readme') {
+        contributors = contributors.slice(0, 10)
+      }
+
+      const tasks = contributors.map(({ login }) => {
+        return function (next) {
+          github.get(`/users/${login}`, function (err, res) {
+            if (err) return next(err)
+
+            const extra = meta[login] || {}
+            const name = extra.name || res.name
+            const social = extra.twitter || extra.mastodon // TODO
+
+            if (!social) {
+              file.warn(`no social profile for @${login}`, null, `${plugin}:social`)
+            }
+
+            next(null, { name, github: login })
+          })
+        }
+      })
+
+      parallel(tasks, function (err, contributors) {
+        if (err) return callback(err)
+
+        injectContributors({ contributors })(root, file)
+        callback()
+      })
+    })
+  }
+}
+
+// Supports:
+// - module (path or id)
+// - nested object: { contributors }
+// - array of contributors
+// - object of contributors (key is assumed to be GitHub username)
+function getMetadata (cwd, file, contributors) {
+  if (contributors == null) {
+    return {}
+  }
+
+  if (typeof contributors === 'string') {
+    contributors = require(resolve.sync(contributors, { basedir: cwd }))
+  }
+
+  if (typeof contributors === 'object') {
+    if (contributors.contributors) {
+      return getMetadata(cwd, file, contributors.contributors)
+    }
+
+    const obj = contributors
+    contributors = []
+
+    for (let [key, contributor] of Object.entries(obj)) {
+      if (!contributor.github) {
+        // Assume that `key` is GitHub username
+        contributor = Object.assign({}, contributor, { github: key })
+      }
+
+      contributors.push(contributor)
+    }
+  }
+
+  const meta = {}
+
+  for (let contributor of contributors) {
+    if (contributor.github) {
+      meta[contributor.github] = contributor
+    } else {
+      const reason = `no github username in ${JSON.stringify(contributor)}`
+      const origin = `${plugin}:require-github-username`
+
+      file.warn(reason, null, origin)
+    }
+  }
+
+  return meta
+}
---
+++ readme.md
@@ -0,0 +1,28 @@
+# remark-gh-contributors [wip]
+
+> Asynchronous [`remark`](https://github.com/remarkjs/remark) plugin to inject GitHub contributors into a markdown table. Pulls top 10 contributors from the GitHub API, augments it with local metadata and calls [`remark-contributors`](https://github.com/hughsk/remark-contributors) to render the markdown table.
+
+[![npm status](http://img.shields.io/npm/v/remark-gh-contributors.svg?style=flat-square)](https://www.npmjs.org/package/remark-gh-contributors)
+[![node](https://img.shields.io/node/v/remark-gh-contributors.svg?style=flat-square)](https://www.npmjs.org/package/remark-gh-contributors)
+[![Travis build status](https://img.shields.io/travis/vweevers/remark-gh-contributors.svg?style=flat-square&label=travis)](http://travis-ci.org/vweevers/remark-gh-contributors)
+[![AppVeyor build status](https://img.shields.io/appveyor/ci/vweevers/remark-gh-contributors.svg?style=flat-square&label=appveyor)](https://ci.appveyor.com/project/vweevers/remark-gh-contributors)
+[![Dependency status](https://img.shields.io/david/vweevers/remark-gh-contributors.svg?style=flat-square)](https://david-dm.org/vweevers/remark-gh-contributors)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square)](https://standardjs.com)
+
+## Usage
+
+## API
+
+### `contributors([options])`
+
+## Install
+
+With [npm](https://npmjs.org) do:
+
+```
+npm install remark-gh-contributors
+```
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT) © 2018-present Vincent Weevers
